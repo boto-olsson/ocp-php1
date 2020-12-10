d09e378e08dcd14b397902823c2ea999942c01e8
boto-olsson <69902826+boto-olsson@users.noreply.github.com>
2020-12-10 13:50:09 +0100

    Update index.php
    Larger font size

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  <head>
  </head>
  <body>
- <?php echo 'App Version: 20.0. Pod IP: '; ?>
+ <?php style="font-size:300%;"echo 'App Version: 20.0. Pod IP: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
  <?php echo ' PHP Version: ' . phpversion(); ?>
  </body>
