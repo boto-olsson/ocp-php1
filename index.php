56aaf6ba8ae9c98add0eaa4a17c6a446aa2415ee
Tobbe Olsson <bolsson@redhat.com>
2020-12-10 14:35:33 +0100

    format changes

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
  </style>
  </head>
  <body>
- <?php style="font-size:300%;"echo 'App Version: 21.0. Pod IP: '; ?>
+ <?php echo 'App Version: 21.0. Pod IP: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
  echo "<br>";
+ <?php echo 'Pod HOSTNAME: '; ?>
  <?php echo $_SERVER['SERVER_NAME']; ?>
  <?php echo ' PHP Version: ' . phpversion(); ?>
  </body>
