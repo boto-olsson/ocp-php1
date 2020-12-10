ddb2041a0afffe6b69affd0298de0d3783666e2e
Tobbe Olsson <bolsson@redhat.com>
2020-12-10 14:46:30 +0100

    mer fix

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
  </head>
  <body>
  <?php echo 'App Version: 21.0. Pod IP: '; ?>
- <?php echo $_SERVER['SERVER_ADDR'] <br>; ?>
- <?php echo 'Pod HOSTNAME: '; ?>
- <?php echo $_SERVER['SERVER_NAME'] <br>; ?>
+ <?php echo $_SERVER['SERVER_ADDR']; ?>
+ <?php echo ' Pod HOSTNAME: '; ?>
+ <?php echo $_SERVER['SERVER_NAME']; ?>
  <?php echo ' PHP Version: ' . phpversion(); ?>
  </body>
 </html>
