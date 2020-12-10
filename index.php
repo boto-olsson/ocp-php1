b162d039f07a42f15aef0e0cab428f7e62bc8d6d
Tobbe Olsson <bolsson@redhat.com>
2020-12-10 14:17:46 +0100

    Added server_hostname and chandged fontsize

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <html>
  <head>
+ <style>
+ body {background-color: gray;}
+ </style>
  </head>
  <body>
- <?php echo 'App Version: 20.0. Pod IP: '; ?>
+ <?php style="font-size:300%;"echo 'App Version: 21.0. Pod IP: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
+ echo "<br>";
+ <?php echo $_SERVER['SERVER_NAME']; ?>
  <?php echo ' PHP Version: ' . phpversion(); ?>
  </body>
 </html>
