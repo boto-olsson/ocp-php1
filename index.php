bb5687092bfb904bf98b2dd67145dc1a24380a47
boto-olsson <69902826+boto-olsson@users.noreply.github.com>
2021-04-21 09:50:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
-<html>
- <head>
- <style>
- body {background-color: gray;}
- </style>
+<head>
+ <title>
+  TEST 
+ </title>
+ <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+ <meta http-equiv="Pragma" content="no-cache" /> 
+ <meta http-equiv="Expires" content="0" />
  </head>
- <body>
- <?php echo 'App Version: 21.0. Pod IP: '; ?>
- <?php echo $_SERVER['SERVER_ADDR']; ?>
+ <html>
+ <body style="background-color: white">
+ <p>
+ <h1>
+ Version 1
+ <br>
+ <?php
+  echo "App Version: 21.0. Pod IP is: ".$_SERVER['SERVER_ADDR'] ;
+  echo "\n<br>" ;
+  date_default_timezone_set('Europe/Stockholm');
+  
  <?php echo ' Pod HOSTNAME: '; ?>
  <?php echo $_SERVER['SERVER_NAME']; ?>
  <?php echo ' PHP Version: ' . phpversion(); ?>
