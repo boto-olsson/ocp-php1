bed5ceff3a308aac3f6bbb7eeb93a5eff6182c17
boto-olsson <69902826+boto-olsson@users.noreply.github.com>
2021-04-21 11:12:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,8 +16,7 @@
   echo "App Version: 21.0. Pod IP is: ".$_SERVER['SERVER_ADDR'] ;
   echo "\n<br>" ;
   date_default_timezone_set('Europe/Stockholm');
-  $date = date('Y/m/d H:i:s' time());
-  echo "\nThe Server time is: " .$date ;
+  echo "The date & time is " . date("Y/m/d h:i:sa");
   echo "\n<br>" ;
   echo "Pod HOSTNAME: "echo .$_SERVER['SERVER_NAME'];
   echo "\n<br>" ;
